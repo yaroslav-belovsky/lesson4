b08d0d92616dd1e9fc49cb5f6fa5c829ec388707
yaroslav-belovsky <yaroslav.belovsky@gmail.com>
2025-05-22 21:02:12 +0300

    1

---
+++ lesson1/lesson1.py
@@ -0,0 +1,9 @@
+
+print("hello \nworld")
+print(1+2+3+1000000)
+print("hello \nworld")
+print(1+2+3+1000000)
+print("hello \nworld")
+print(1+2+3+1000000)
+print("hello \nworld")
+print(1+2+3+1000000)
