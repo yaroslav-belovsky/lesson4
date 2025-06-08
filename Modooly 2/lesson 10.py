d9619f66b9b1ff08c20ae7495360e48d78430917
yaroslav-belovsky <yaroslav.belovsky@gmail.com>
2025-06-08 19:13:19 +0300

    Home Work

---
+++ Modooly 2/lesson 10.py
@@ -0,0 +1,14 @@
+my_kontakt = {}
+k = 0
+print("1 + контакт\n2 пошук\n3 видалити")
+komanda = input("ваша команда: ")
+if komanda == "1":
+    k = input("додавай: ")
+    d = input("тепер номер: ")
+    my_kontakt[k] = d
+elif komanda == "2":
+    k = input("назва контакта: ")
+    b = my_kontakt.get(k)
+else:
+    k = input("що видалити: ")
+    del my_kontakt[k]
