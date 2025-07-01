566f8d4d7277174e6192c0f7d705bc7db5084382
yaroslav-belovsky <yaroslav.belovsky@gmail.com>
2025-07-01 11:45:12 +0300

    Home Work

--- Modooly 2/final PROEKT moduly 2.py
+++ Modooly 2/final PROEKT moduly 2.py
@@ -1,5 +1,5 @@
 pruholosni = ["б", "в", "г", "ґ", "д", "ж", "з", "й", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ"]
-holosni = ["а", "о", "у", "е", "и", "і", "я"]
+holosni = ["а", "о", "у", "е", "и", "і", "я", "ю", "є", "ї"]
 while True:
     prh = 0
     hol = 0
@@ -7,8 +7,11 @@ while True:
     sumwol = 0
     inha_mowa = 0
     cufru = 0
-    text = input("напиши текст украюнською: ")
-    if text == "":
+    pos = 0
+    start = 0
+    sl = 0
+    text = input("напиши текст українською: ") + ' '
+    if text == " ":
         print("ти нічого не написав")
         continue
     for i in text:
@@ -18,13 +21,17 @@ while True:
             hol += 1
         elif i == " ":
             prob += 1
+            if pos != start:
+                print(text[start:pos], '!!!', start, pos)
+                sl += 1
+            start = pos + 1
         elif i == "`" or i == "~" or i == "<" or i == ">" or i == "." or i == "," or i == "?" or i == "!" or i == "(" or i == ")" or i == "/" or i == "\\" or i == "*" or i == "-" or i == "+":
             sumwol += 1
         elif i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9" or i == "0":
             cufru += 1
         else:
             inha_mowa += 1
-    sl = prob + 1
+        pos += 1
     print(f"слів: {sl}")
     print(f"приголосниг: {prh}")
     print(f"голосних: {hol}")
