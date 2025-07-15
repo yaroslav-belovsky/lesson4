0ae070caadb7265448bed287add8b43f9648483a
Yaroslav <yaroslav.belovsky@gmail.com>
2025-07-15 18:36:44 +0300

    1

---
+++ Modooly 3/DZ 37.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+def f():
+    name = Entry.get()
+    label.config(text=f"Привіт {name}!")
+root =tk.Tk()
+root.geometry("1800x900")
+root.title("вітання")
+label = tk.Label(root, font=("Arial", 30), text="напиши ім'я і натисни пиривітатись щоб я нписав привітння", fg="blue", bg="yellow")
+label.pack()
+Entry = tk.Entry(root, font=("Arial", 30))
+Entry.pack()
+Button = tk.Button(root, text="Привітатись", command=f)
+Button.pack()
+root.mainloop()
