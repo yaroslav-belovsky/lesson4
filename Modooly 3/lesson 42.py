f38f7cde6e39f602bf6e12d88738742f66a20187
Yaroslav <yaroslav.belovsky@gmail.com>
2025-07-17 11:37:02 +0300

    1

--- Modooly 3/lesson 42.py
+++ Modooly 3/lesson 42.py
@@ -2,3 +2,9 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Awto Clicker")
 root.geometry("300x300")
+root.resizable(False, False)
+
+label = tk.Label(root, text="Awto Clicker")
+label.pack()
+
+root.mainloop()
