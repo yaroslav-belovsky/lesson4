bdee0eab358734a0fdac16898ab861da0467c7ff
yaroslav-belovsky <yaroslav.belovsky@gmail.com>
2025-07-28 22:31:28 +0300

    1

--- Modooly 3/lesson 48.py
+++ Modooly 3/lesson 48.py
@@ -3,6 +3,8 @@ import customtkinter as ctk
 BTC_TO_UAH = float(2000000) # Наприклад, 1 BTC = 110 0000 UAH
 ETH_TO_UAH = float(112000) # Наприклад, 1 ETH = 112 000 UAH
 USDT_TO_UAH = float(38) # Наприклад, 1 USDT = 38 UAH
+EURO_TO_UAH = float(49)
+USD_TO_UAH = float(41.77)
 
 def convert():
     try:
@@ -66,6 +68,43 @@ def convert():
             if to_value == "UAH":
                 result = zn * 1
 
+        if from_value == "EURO":
+            if to_value == "BTC":
+                result = BTC_TO_UAH / EURO_TO_UAH * zn
+
+            if to_value == "ETH":
+                result = ETH_TO_UAH / EURO_TO_UAH * zn
+
+            if to_value == "USDT":
+                result = EURO_TO_UAH / USDT_TO_UAH * zn
+
+            if to_value == "UAH":
+                result = EURO_TO_UAH * zn
+
+            if to_value == "EURO":
+                result = EURO_TO_UAH / EURO_TO_UAH * zn
+
+            if to_value == "USD":
+                result = USD_TO_UAH / EURO_TO_UAH * zn
+
+
+        if from_value == "USD":
+            if to_value == "BTC":
+                result = zn * BTC_TO_UAH
+
+            if to_value == "ETH":
+                result = zn * ETH_TO_UAH
+
+            if to_value == "USDT":
+                result = zn * USDT_TO_UAH
+
+            if to_value == "UAH":
+                result = USD_TO_UAH
+
+            if to_value == "EURO":
+                result = USD_TO_UAH / EURO_TO_UAH * zn
+
+
         result_label.configure(text=f"{zn} {from_value} = {result} {to_value}")
     except ValueError:
         result_label.configure(text="це не число!")
@@ -89,12 +128,12 @@ entry_amount.pack(pady=10)
 
 # Вибір валюти для конвертації з
 from_currency_var = ctk.StringVar(value="BTC")
-from_currency_menu = ctk.CTkOptionMenu(app, variable=from_currency_var, values=["BTC", "ETH", "USDT", "UAH"])
+from_currency_menu = ctk.CTkOptionMenu(app, variable=from_currency_var, values=["BTC", "ETH", "USDT", "UAH", "EURO", "USD"])
 from_currency_menu.pack(pady=5)
 
 # Вибір валюти для конвертації в
 to_currency_var = ctk.StringVar(value="UAH")
-to_currency_menu = ctk.CTkOptionMenu(app, variable=to_currency_var, values=["BTC", "ETH", "USDT", "UAH"])
+to_currency_menu = ctk.CTkOptionMenu(app, variable=to_currency_var, values=["BTC", "ETH", "USDT", "UAH", "EURO", "USD"])
 to_currency_menu.pack(pady=5)
 
 # Кнопка конвертації
